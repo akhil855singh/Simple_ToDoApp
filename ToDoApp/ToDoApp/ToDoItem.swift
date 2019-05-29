dad0cf34c7b1a4cfa8d3bf187c5f7942c2e39259
Akhil Singh <akhil855singh@gmail.com>
2019-05-29 09:59:37 +0530

    Added TodoItem Model
    Added TodoItem Model

---
+++ ToDoApp/ToDoApp/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDoApp
+//
+//  Created by Akhil Singh on 28/05/19.
+//  Copyright © 2019 Akhil Singh. All rights reserved.
+//
+
+import Foundation
+
+class ToDoItem{
+    var text = ""
+    var checked = false
+}
--- ToDoApp/ToDoApp/ToDoViewController.swift
+++ ToDoApp/ToDoApp/ToDoViewController.swift
@@ -10,51 +10,122 @@ import UIKit
 
 class ToDoViewController: UITableViewController {
 
+    var row0:ToDoItem
+    var row1:ToDoItem
+    var row2:ToDoItem
+    var row3:ToDoItem
+    var row4:ToDoItem
+
+    
+    required init?(coder aDecoder: NSCoder) {
+        row0 = ToDoItem()
+        row0.text = "Take a jog"
+        
+        row1 = ToDoItem()
+        row1.text = "Watch a movie"
+        
+        row2 = ToDoItem()
+        row2.text = "Code an app"
+        
+        row3 = ToDoItem()
+        row3.text = "Walk the dog"
+        
+        row4 = ToDoItem()
+        row4.text = "Study design patterns"
+        
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1000
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellIdentifier = "ToDoItem"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
         
+        
         if let label = cell.viewWithTag(100) as? UILabel{
-            let value = indexPath.row % 5
+            
             var labelText = ""
-            switch value{
+            switch indexPath.row{
             case 0:
-                labelText = "Take a jog"
+                labelText = row0.text
             case 1:
-                labelText = "Watch a movie"
+                labelText = row1.text
             case 2:
-                labelText = "Code an app"
+                labelText = row2.text
             case 3:
-                labelText = "Walk the dog"
+                labelText = row3.text
             case 4:
-                labelText = "Study design patterns"
+                labelText = row4.text
             default:
                 labelText = "Sleep"
+                }
+                label.text = labelText
             }
-            label.text = labelText
-        }
+        configureCheckmark(for: cell, and: indexPath)
+
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at: indexPath){
-            if cell.accessoryType == .none{
-                cell.accessoryType = .checkmark
-            }
-            else{
-                cell.accessoryType = .none
-            }
+            configureCheckmark(for: cell, and: indexPath)
         }
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    fileprivate func configureCheckmark(for cell:UITableViewCell, and indexPath:IndexPath){
+        switch indexPath.row{
+        case 0:
+            if row0.checked{
+                cell.accessoryType = .none
+            }
+            else{
+                cell.accessoryType = .checkmark
+            }
+            row0.checked = !row0.checked
+        case 1:
+            if row1.checked{
+                cell.accessoryType = .none
+            }
+            else{
+                cell.accessoryType = .checkmark
+            }
+            row1.checked = !row1.checked
+        case 2:
+            if row2.checked{
+                cell.accessoryType = .none
+            }
+            else{
+                cell.accessoryType = .checkmark
+            }
+            row2.checked = !row2.checked
+        case 3:
+            if row3.checked{
+                cell.accessoryType = .none
+            }
+            else{
+                cell.accessoryType = .checkmark
+            }
+            row3.checked = !row3.checked
+        case 4:
+            if row4.checked{
+                cell.accessoryType = .none
+            }
+            else{
+                cell.accessoryType = .checkmark
+            }
+            row4.checked = !row4.checked
+        default:
+            break
+        }
     }
 
 }
